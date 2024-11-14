12cdd3dfb64908afbdcd2bbb322680af4ad8bc45
Ivan Toropygin <itori86@mail.ru>
2024-11-15 01:32:41 +0300

    2-3. email and sender

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,12 +1,50 @@
 package otus.gpb.homework.activities.receiver
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReceiverActivity : AppCompatActivity() {
 
+    private lateinit var titleTextView: TextView
+    private lateinit var yearTextView: TextView
+    private lateinit var descriptionTextView: TextView
+    private lateinit var posterImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        titleTextView = findViewById(R.id.titleTextView)
+        yearTextView = findViewById(R.id.yearTextView)
+        descriptionTextView = findViewById(R.id.descriptionTextView)
+        posterImageView = findViewById(R.id.posterImageView)
+
+        val intent = intent
+        if (intent.action == Intent.ACTION_SEND) {
+            val title = intent.getStringExtra(TITLE_KEY)
+            val year = intent.getStringExtra(YEAR_KEY)
+            val description = intent.getStringExtra(DESCRIPTION_KEY)
+
+            titleTextView.text = title
+            yearTextView.text = year
+            descriptionTextView.text = description
+
+            val posterResId = when (title) {
+                "Славные парни" -> R.drawable.niceguys
+                else -> R.drawable.interstellar
+            }
+
+            posterImageView.setImageDrawable(getDrawable(posterResId))
+        }
+    }
+
+    companion object {
+        const val TITLE_KEY = "TITLE_KEY"
+        const val YEAR_KEY = "YEAR_KEY"
+        const val DESCRIPTION_KEY = "DESCRIPTION_KEY"
     }
 }
+
--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,19 +23,74 @@ class SenderActivity : AppCompatActivity() {
         }
 
         val buttonOpenMaps: Button = findViewById(R.id.to_maps_button)
+        val buttonSendEmail: Button = findViewById(R.id.send_email_button)
+        val buttonOpenReceiver: Button = findViewById(R.id.open_receiver_button)
 
         buttonOpenMaps.setOnClickListener {
             openGoogleMaps()
         }
+
+        buttonSendEmail.setOnClickListener {
+            sendEmail()
+        }
+
+        buttonOpenReceiver.setOnClickListener {
+            openReceiver()
+        }
+    }
+
+    private fun openReceiver() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            addCategory(Intent.CATEGORY_DEFAULT)
+            intent.putExtra(TITLE_KEY, "Славные парни")
+            intent.putExtra(YEAR_KEY, "2016")
+            intent.putExtra(
+                DESCRIPTION_KEY,
+                """
+                    Что бывает, когда напарником брутального костолома становится субтильный лопух?
+                     Наемный охранник Джексон Хили и частный детектив Холланд Марч вынуждены работать
+                      в паре, чтобы распутать плевое дело о пропавшей девушке, которое оборачивается
+                       преступлением века. Смогут ли парни разгадать сложный ребус, если у каждого
+                        из них – свои, весьма индивидуальные методы.
+                """.trimIndent()
+            )
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Нет приложения для обработки Intent", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun sendEmail() {
+        val to = "android@otus.ru"
+        val theme = "Hello from Kotlin!"
+        val message = "This is a test email sent from my Kotlin application."
+
+        val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:$to")
+            putExtra(Intent.EXTRA_SUBJECT, theme)
+            putExtra(Intent.EXTRA_TEXT, message)
+        }
+
+        startActivity(emailIntent)
     }
 
     private fun openGoogleMaps() {
         val latitudeMoscow = 55.7558
         val longitudeMoscow = 37.6173
-        val restaurants = "Рестораны"
+        val restaurants = "Restaurants"
         val uri = Uri.parse("geo:$latitudeMoscow,$longitudeMoscow?q=$restaurants")
         val intent = Intent(Intent.ACTION_VIEW, uri)
         intent.setPackage("com.google.android.apps.maps")
         startActivity(intent)
+    }
+
+    companion object {
+        const val TITLE_KEY = "TITLE_KEY"
+        const val YEAR_KEY = "YEAR_KEY"
+        const val DESCRIPTION_KEY = "DESCRIPTION_KEY"
     }
 }
